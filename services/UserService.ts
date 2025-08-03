6476a790a8be831838e5df1d6f229e42bf7e49a4
Toky <toky.andrianina401@gmail.com>
2025-08-03 18:48:19 +0300

    update profile endpoints

--- services/UserService.ts
+++ services/UserService.ts
@@ -2,7 +2,7 @@ export default class UserService {
     static API_URL: string = process.env.EXPO_PUBLIC_API_BASEURL || '';
 
     public static async getUserById(userId: any, token: any): Promise<any> {
-        return fetch(`${this.API_URL}/api/users/profile/${userId}`, {
+        return fetch(`${this.API_URL}/api/users/me`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
