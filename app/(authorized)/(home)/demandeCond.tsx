7909957406d9cea0ea5fa02bd4745bce5750b4b5
Arinomena <nomena.ratsira@gmail.com>
2025-11-06 15:21:33 +0300

    test p2p

---
+++ app/(authorized)/(home)/demandeCond.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function DemandeCond() {
+  return (
+    <View>
+      <Text>Écran Demande Conducteur</Text>
+    </View>
+  );
+}
