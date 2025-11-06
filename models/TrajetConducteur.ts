6192676d1b65e118986324a2cdc4c9be9e05fd2a
sarobidyrakotoson <sarobidyrakoto04@gmail.com>
2025-11-06 20:03:53 +0300

    ajout jours

--- models/TrajetConducteur.ts
+++ models/TrajetConducteur.ts
@@ -1,17 +1,20 @@
-
 export interface TrajetConducteur {
-    id: string;
-    idConducteur: string;
-    pointDepart: string;
-    latDepart: number;
-    lngDepart: number;
-    pointArrivee: string;
-    latArrivee: number;
-    lngArrivee: number;
-    heureDepartEstimee: string; 
-    placesDisponibles: number;
-    description: string;
-    statut: string;
-    actif: number;
-    voitureId: string;
+  id: string;
+  idConducteur: string;
+  pointDepart: string;
+  latDepart: number;
+  lngDepart: number;
+  pointArrivee: string;
+  latArrivee: number;
+  lngArrivee: number;
+  heureDepartEstimee: string;
+  placesDisponibles: number;
+  placesDispoJournalier?: number | null;
+  description: string;
+  statut: string;
+  actif: number;
+  voitureId: string;
+  jours?: number[] | null;
+  dateDesactivationDebut?: string | null;
+  dateDesactivationFin?: string | null;
 }
